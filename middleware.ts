091a63c70ce833e3cad6c8d00c61988020c8a65c
him-it <it@heartinmotion.org>
2024-11-19 14:27:22 -0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -27,8 +27,6 @@ export default auth((req) => {
     const isBasicAdminRoute = basicAdminRoutes.includes(nextUrl.pathname)
     const isSuperAdminRoute = superAdminRoutes.includes(nextUrl.pathname)
 
-    console.log(Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl)))
-    
     if(isApiAuthRoutes)
         return
 
@@ -38,8 +36,6 @@ export default auth((req) => {
         return
     }
 
-    console.log(Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl)))
-    
     if(isNonMemberRoute) {
         if(!isLoggedIn || adminLevel !== undefined)
             return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
