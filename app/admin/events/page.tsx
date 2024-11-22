2a084891ed872c0e88316fe945638642cf09e4b9
hit-it <it@heartinmotion.org>
2024-11-22 03:35:40 -0800

    asdf

--- app/admin/events/page.tsx
+++ app/admin/events/page.tsx
@@ -4,24 +4,20 @@ import { getEvents } from "@/actions/admin/event"
 import { AdminPageWrapper } from "@/components/admin/adminPageWrapper"
 import AdminEventsList from "@/components/admin/events/eventsList"
 import { Prisma } from "@prisma/client"
-import { useSession } from "next-auth/react"
 import { useEffect, useState } from "react"
 
 const Admin_EventsListPage = () => {
     const [ eventData, setEventData ] = useState<Prisma.PromiseReturnType<typeof getEvents>>()
-    const session = useSession()
 
     useEffect(() => {
         const fetchEvents = async () => {
             await getEvents()
             .then(res => {
                 setEventData(res)
-                console.log(res)
-                console.log("hi")
             })
         }
         fetchEvents()
-    }, [session])
+    }, [])
 
     return (
         <AdminPageWrapper title="Events" redirect="/admin">
