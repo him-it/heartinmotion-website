d4e3bc63e028b3cc4b55f3d3d602ba94fb53cdab
hit-it <it@heartinmotion.org>
2024-11-22 01:18:34 -0800

    adsf

--- app/admin/files/page.tsx
+++ app/admin/files/page.tsx
@@ -3,10 +3,21 @@
 import { getFiles } from "@/actions/volunteer/file"
 import { AdminPageWrapper } from "@/components/admin/adminPageWrapper"
 import AdminFilesList from "@/components/admin/files/filesList"
+import { Prisma } from "@prisma/client"
+import { useEffect, useState } from "react"
 
 const Admin_FilesListPage = async () => {
-    const fileData = await getFiles()
-
+    const [fileData, setFileData] = useState<Prisma.PromiseReturnType<typeof getFiles>>()
+    useEffect(() => {
+        const fetchFiles = async () => {
+            await getFiles()
+            .then((res) => {
+                setFileData(res)
+            })
+        }
+        fetchFiles()
+    }, [])
+    
     return (
         <AdminPageWrapper title="Files" redirect="/admin">
             <AdminFilesList fileData={JSON.parse(JSON.stringify(fileData))}></AdminFilesList>
