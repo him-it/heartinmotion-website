6aa2a6e8194a35e1f5f818a27dfea8e005e2702b
hit-it <it@heartinmotion.org>
2024-11-22 03:10:10 -0800

    ASdf

--- app/admin/events/page.tsx
+++ app/admin/events/page.tsx
@@ -3,15 +3,23 @@
 import { getEvents } from "@/actions/admin/event"
 import { AdminPageWrapper } from "@/components/admin/adminPageWrapper"
 import AdminEventsList from "@/components/admin/events/eventsList"
+import { Prisma } from "@prisma/client"
 
-const Admin_EventsListPage = async () => {
-    const eventData = await getEvents()
-
+const Admin_EventsListPage = ({ eventsData }: { eventsData: Prisma.PromiseReturnType<typeof getEvents> }) => {
     return (
         <AdminPageWrapper title="Events" redirect="/admin">
-           <AdminEventsList eventsData={eventData} />
+           <AdminEventsList eventsData={eventsData} />
         </AdminPageWrapper>
     )
+}
+
+export async function getServerSideProps() {
+    try {
+        const events = await getEvents()
+        return { props: { eventsData: events } }
+    } catch (error) {
+        return { props: { eventsData: undefined } }
+    }
 }
 
 export default Admin_EventsListPage
